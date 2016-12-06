763653fdebf503f4723f46bdf96d05cacbe0e25a
Maciej Rapacz <maciek.rapacz@gmail.com>
2016-12-06 14:52:39 +0100

    Add probe model

--- raspberry/weatherserver/temperatures/models.py
+++ raspberry/weatherserver/temperatures/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Probe(models.Model):
+    node_id = models.SmallIntegerField()
+    temperature = models.FloatField()
+    timestamp = models.DateTimeField(default=datetime.now)
