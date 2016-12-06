925da4fd7e7366f5d6b65a00299d76b50b55932f
Maciej Rapacz <maciek.rapacz@gmail.com>
2016-12-06 12:24:51 +0100

    Remove redundant debug statements

--- client.py
+++ client.py
@@ -7,7 +7,6 @@ import json
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     for node in config.node_ids:
-        print("nodemcu/" + node)
         client.subscribe("nodemcu/" + node)
 
 
@@ -17,9 +16,9 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
-
+    # print(msg.topic + " " + str(msg.payload))
     message = msg.payload.decode()
+    print(message)
     # dump_message(message)
 
 
