110712b8785bc02c5f7a5c50c430f6a9347ae020
Maciej Rapacz <maciek.rapacz@gmail.com>
2016-12-06 23:20:13 +0100

    Change chip id field to integer field from smallinteger

--- raspberry/weatherserver/temperatures/models.py
+++ raspberry/weatherserver/temperatures/models.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 
 class Probe(models.Model):
-    node_id = models.SmallIntegerField()
+    node_id = models.IntegerField()
     temperature = models.FloatField()
     timestamp = models.DateTimeField(default=datetime.now)
