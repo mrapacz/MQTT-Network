11dcc7d69b9f9ad80150a353a0ed5452606ff15c
Maciej Rapacz <maciek.rapacz@gmail.com>
2016-12-08 21:41:02 +0100

    Add simple to string to probe model

--- raspberry/weatherserver/temperatures/models.py
+++ raspberry/weatherserver/temperatures/models.py
@@ -7,3 +7,6 @@ class Probe(models.Model):
     node_id = models.IntegerField()
     temperature = models.FloatField()
     timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return " ".join((str(x) for x in [self.node_id, self.temperature, self.timestamp]))
