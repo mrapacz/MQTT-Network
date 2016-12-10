cfcdadd0b35349e937667c692a37473b4bf66238
Maciej Rapacz <maciek.rapacz@gmail.com>
2016-12-10 15:48:45 +0100

    Add drawing charts

--- raspberry/weatherserver/temperatures/views.py
+++ raspberry/weatherserver/temperatures/views.py
@@ -1,9 +1,42 @@
-from django.http import HttpResponse
+from datetime import datetime, timedelta
+from chartit import Chart
+from chartit import DataPool
 from django.shortcuts import render
+from django.utils import timezone
 
 from temperatures.models import Probe
 
 
 def index(request):
+    print(timezone.now())
+    ds = DataPool(
+        series=
+        [{
+            'options': {'source': Probe.objects.all().filter(timestamp__gte=timezone.now() - timedelta(days=1))},
+            'terms':
+                [
+                    'timestamp',
+                    'temperature',
+                ]
+        }])
+
+    cht = Chart(
+        datasource=ds,
+        series_options=
+        [{
+            'options':
+                {
+                    'type': 'line',
+                    'stacking': False,
+                },
+            'terms': {'timestamp': ['temperature', ]}
+        }],
+        chart_options=
+        {
+            'title': {'text': 'Temperature chart'},
+            'xAxis': {'title': {'text': 'Time'}}
+        },
+        x_sortf_mapf_mts=(None, lambda timestamp: datetime.strftime(timestamp, "%Y-%m-%d %H:%M:%S"), False)
+    )
     probes = Probe.objects.order_by('-timestamp')
-    return render(request, 'temperatures/index.html', {'probes': probes})
+    return render(request, 'temperatures/index.html', {'probes': probes, 'chart': cht})
--- raspberry/weatherserver/weatherserver/settings.py
+++ raspberry/weatherserver/weatherserver/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'jquery',  # for chartit app
+    # 'highcharts',  # for chartit app
+    'chartit',
     'temperatures',
 ]
 
