30469d891ecfcd5207ed9b4991d348af35180f31
Maciej Rapacz <maciek.rapacz@gmail.com>
2016-12-28 22:42:45 +0100

    Round to the closest 20-minutes while saving to db

--- raspberry/weatherserver/utils/dumper.py
+++ raspberry/weatherserver/utils/dumper.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 def dump_data(node_id, temperature):
     time = timezone.now()
-    time = time.replace(second=0, microsecond=0)
+    time = time.replace(minute=time.minute - time.minute % 20, second=0, microsecond=0)
     p = Probe(node_id=node_id, temperature=temperature, timestamp=time)
     p.save()
     print(p)
