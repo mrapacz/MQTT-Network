fd1ecc76140daf4a0003c46f76b87fe550537201
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-01-06 09:47:09 +0100

    Store one probe in 10 minutes

--- raspberry/weatherserver/utils/dumper.py
+++ raspberry/weatherserver/utils/dumper.py
@@ -1,11 +1,20 @@
+from statistics import mean
+
 from django.utils import timezone
-from weatherserver.utils.dumper_setup import Probe
+from weatherserver.utils.dumper_setup import Probe, DoesNotExist, MultipleObjectsReturned
 from datetime import datetime
 
 
 def dump_data(node_id, temperature):
     time = timezone.now()
-    time = time.replace(minute=time.minute - time.minute % 20, second=0, microsecond=0)
-    p = Probe(node_id=node_id, temperature=temperature, timestamp=time)
-    p.save()
-    print(p)
+    time = time.replace(minute=(time.minute - time.minute % 10), second=0, microsecond=0)
+
+    try:
+        p = Probe.objects.get(timestamp=time)
+        p.temperature = mean(temperature, p.temperature)
+        p.save()
+    except DoesNotExist:
+        p = Probe(node_id=node_id, temperature=temperature, timestamp=time)
+        p.save()
+    except MultipleObjectsReturned:
+        print("More than two probes during the last 10 minutes!")
